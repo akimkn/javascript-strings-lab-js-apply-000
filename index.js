dcd50b08decccf01c0df828bd07165342d404b59
Austin Kim <ask199416@gmail.com>
2017-11-02 21:28:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,4 +8,4 @@ var greetSpecialGuest = standardGreet + ", " + specialGuest + "!";
 
 var topic = "space";
 
-var conversation = `Let's talk about ${topic}`;
+var conversation = `Let's talk about ${topic}.`;
